e250eda7406c620842315fdbd644e1bca54415ba
marouanedbibih <dbibih.marouane@emsi-edu.ma>
2024-01-21 16:27:05 +0100

    Classe Complexe Ex 2 Tp 7

--- src/interfaces/ex2/Main.java
+++ src/interfaces/ex2/Main.java
@@ -2,7 +2,18 @@ package interfaces.ex2;
 
 public class Main {
     public static void main(String[] args) {
+        Complexe c1 = new Complexe(4,12);
+        Complexe c2 = new Complexe(-3,6);
+        Complexe c3,c4;
 
+        // Affichage de Complexe
+        System.out.println(c1.affiche());
+        System.out.println(c2.affiche());
+
+        c3 = (Complexe) (c1.plus(c2));
+        c4 = (Complexe) (c1.moins(c2));
+        System.out.println( c3.affiche());
+        System.out.println(c4.affiche());
 
     }
 }
